72513e06b8934fc6c2d39ff2802a1db35b13bda0
itsmesijan <itsmesijanregmi@gmail.com>
2022-04-14 12:55:06 +0545

    C Programs

---
+++ Area of Circle.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+#define PIE 3.14
+int main()
+{
+	
+float a,radius;
+
+printf("Enter The Radius Of Circle \n");
+scanf("%f",&radius);
+a=PIE*radius*radius;
+
+printf("Area of circle is =%.2f",a);
+	
+		
+	
+}
---
+++ Simplae Intrest.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+	
+int si,p,t,r;
+
+printf("Enter p, t, r  \n");
+scanf("%d%d%d",&p,&t,&r);
+
+	si=(p*t*r)/100;
+	printf("\n SI is %d",si);
+	
+	getch();
+}

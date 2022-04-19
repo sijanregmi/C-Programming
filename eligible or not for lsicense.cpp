b751dcf5276a72bc6c50efce2e110c953a60218a
itsmesijan <itsmesijanregmi@gmail.com>
2022-04-19 12:33:35 +0545

    Added some programs

---
+++ eligible or not for lsicense.cpp
@@ -0,0 +1,23 @@
+
+// Program to input the age of the user and to know if he/she is eligible for liscense 
+
+#include<stdio.h>
+ main()
+{	
+int age;
+printf("Enter Your Age");
+scanf("%d",&age);
+
+if (age>=16)
+{
+	
+	printf("you are elligible for liscense");
+}
+else
+{
+	
+	printf("you are not elligible for liscense");
+}
+
+	
+}

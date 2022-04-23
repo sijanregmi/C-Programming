ace939075bf9d902175c6c12e711597f8418ff2a
itsmesijan <itsmesijanregmi@gmail.com>
2022-04-23 22:48:40 +0545

    new program

---
+++ Program to find the number is even or odd by if else.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+	int a;
+	printf("Enter a number");
+	scanf("%d",&a);	
+	if (a%2==0)
+	{
+		printf("Even Number");
+	}
+	else
+	{
+		printf("Odd Number");
+	}		
+}
---
+++ test.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h> 
+main ()
+{	
+int age;
+	printf("Enter You Age For Your Liscense\n"); 	
+	scanf("%d",&age);	
+	if (age>=16)
+	{				
+		printf("Congrulations You Are Eligible For Liscense\n");
+	}	
+	else
+	{	
+		printf("Sorry to Say but you are not eligible for Liscense\n");
+	}	
+}
+
+
+
